e8aaa738440bebead72f1eaa1143ea96a7bdfa08
Martin Basterrechea <mbs@reshapebiotech.com>
2022-11-05 19:45:20 +0200

    prettier

--- chrome_extension/netflix.js
+++ chrome_extension/netflix.js
@@ -1,94 +1,117 @@
 const tvModels = {
-    "LG OLED55BXPUA": [106, 295],
-    "TCL 65P610": [120, 190],
-    "LG 75UN7070PUC": [110, 240],
-    "Samsung QN55Q80AAFXZA": [88, 220],
-    "Samsung UN86TU9000FXZA": [120, 285],
-    "Samsung QN75QN900AFXZC": [234, 475],
-    "Samsung QN43Q60BAFXZC": [50, 115],
-    "Samsung QN85QN85AAFXZC": [104, 315],
-    "LG 60UN7000PUB": [83.9, 175],
-    "LG 82UN8570PUC": [158, 315],
-    "LG 55UN7000PUB": [68.6, 140],
-    "TCL 50P610": [73, 130],
-    "TCL 55P815": [87, 140]
-}
-
-
+  "LG OLED55BXPUA": [106, 295],
+  "TCL 65P610": [120, 190],
+  "LG 75UN7070PUC": [110, 240],
+  "Samsung QN55Q80AAFXZA": [88, 220],
+  "Samsung UN86TU9000FXZA": [120, 285],
+  "Samsung QN75QN900AFXZC": [234, 475],
+  "Samsung QN43Q60BAFXZC": [50, 115],
+  "Samsung QN85QN85AAFXZC": [104, 315],
+  "LG 60UN7000PUB": [83.9, 175],
+  "LG 82UN8570PUC": [158, 315],
+  "LG 55UN7000PUB": [68.6, 140],
+  "TCL 50P610": [73, 130],
+  "TCL 55P815": [87, 140],
+};
 
 function calculateTitleScore(tvData, movieData, price_per_hour) {
-    let min_power_consumption = tvData[0];
-    let max_power_consumption = tvData[1];
-    let pixels_cost = movieData.pixels_cost;
-    let movie_length_hours = movieData.runtime_hours;
-    return (
-        min_power_consumption + 
-            (max_power_consumption - min_power_consumption) * pixels_cost
-        / (2850.0 * 480)
-    ) * movie_length_hours / 1000 * price_per_hour;
+  let min_power_consumption = tvData[0];
+  let max_power_consumption = tvData[1];
+  let pixels_cost = movieData.pixels_cost;
+  let movie_length_hours = movieData.runtime_hours;
+  return (
+    (((min_power_consumption +
+      ((max_power_consumption - min_power_consumption) * pixels_cost) /
+        (2850.0 * 480)) *
+      movie_length_hours) /
+      1000) *
+    price_per_hour
+  );
 }
 
-
 async function insertScoreOnThumbnail(element, tvData, elPrice) {
-    let title = element.getAttribute('aria-label');
-    let movieData = movieDatabase[title];
-    let score;
-    if (movieData) {
-        score = await calculateTitleScore(tvData, movieData, elPrice);
-    } else {
-        score = 'N/A';
-    }
-    let scoreElement = document.createElement('div');
-    scoreElement.innerHTML = score;
-    element.appendChild(scoreElement);
-    element.classList.add('movieZap-score');
-    // scoredTitles.add(title);
+  let title = element.getAttribute("aria-label");
+  let movieData = movieDatabase[title];
+  let score;
+  if (movieData) {
+    score = await calculateTitleScore(tvData, movieData, elPrice);
+  } else {
+    score = "N/A";
+  }
+  let scoreElement = document.createElement("div");
+  scoreElement.innerHTML = score;
+  element.appendChild(scoreElement);
+  element.classList.add("movieZap-score");
+  // scoredTitles.add(title);
 }
 
 function getAllTitleElements() {
-    return document.querySelectorAll('.title-card [aria-label]:not(.movieZap-score)');
+  return document.querySelectorAll(
+    ".title-card [aria-label]:not(.movieZap-score)"
+  );
 }
 
 let scoringInProgress = false;
 let scoredTitles = new Set();
+let scoringPrice = 0.1; // If the price or model changes, re-score all titles
+let scoringTvModel = "LG OLED55BXPUA";
 
 async function scoreAllTitles() {
-    if (!scoringInProgress) {
-        console.log('Scoring in progress...');
-        scoringInProgress = true;
+  if (!scoringInProgress) {
+    console.log("Scoring in progress...");
+    scoringInProgress = true;
+    chrome.storage.sync.get(["elPrice", "tvModel"], async function (data) {
+      let resetScore = false;
+      if (scoringPrice != data.elPrice || scoringTvModel != data.tvModel) {
+        console.log("Price or model changed, re-scoring all titles");
+        resetScore = true;
+        scoringPrice = data.elPrice;
+        scoringTvModel = data.tvModel;
+      }
+      // Detect all titles on the page
+      let titles = getAllTitleElements(force);
 
-        // Detect all titles on the page
-        let titles = getAllTitleElements();
-        // Ignore titles that have already been scored.
-        // titles = Array.from(titles).filter(title => !scoredTitles.has(title.getAttribute('aria-label')));
-        console.log("Found " + titles.length + " new titles.");
-        if (titles.length > 0) {
-            chrome.storage.sync.get(['elPrice', 'tvModel'], async function(data) {
-                let tvData = tvModels[data.tvModel];
-                let elPrice = data.elPrice;
-                // Score all new titles
-                for (let i = 0; i < titles.length; i++) {
-                    let title_element = titles[i];
-                    await insertScoreOnThumbnail(title_element, tvData, elPrice);
-                }
-            });
+      // Test database connection
+      let url = "http://34.88.229.226:3000/movies";
+      console.log("Testing database connection...");
+      chrome.runtime.sendMessage(
+        {
+          contentScriptQuery: "getData",
+          url: url,
+        },
+        function (jsonData) {
+          console.log("got data");
+          console.log(jsonData);
         }
+      );
 
-        scoringInProgress = false;
-        console.log('Scoring done.');
-    }
+      // Ignore titles that have already been scored.
+      // titles = Array.from(titles).filter(title => !scoredTitles.has(title.getAttribute('aria-label')));
+      console.log("Found " + titles.length + " new titles.");
+      if (titles.length > 0) {
+        let tvData = tvModels[scoringTvModel];
+        // Score all new titles
+        for (let i = 0; i < titles.length; i++) {
+          let title_element = titles[i];
+          await insertScoreOnThumbnail(title_element, tvData, scoringPrice);
+        }
+      }
 
+      scoringInProgress = false;
+      console.log("Scoring done.");
+    });
+  }
 }
 
 (async function main() {
-    // Run every 2 seconds
-    const scoringFunction = () => {
-        try {
-            scoreAllTitles();
-        } catch (e) {
-	        console.error(e);
-        }
-        setTimeout(scoringFunction, 2000);
-    };
-    setTimeout(scoringFunction, 100);
+  // Run every 2 seconds
+  const scoringFunction = () => {
+    try {
+      scoreAllTitles();
+    } catch (e) {
+      console.error(e);
+    }
+    setTimeout(scoringFunction, 2000);
+  };
+  setTimeout(scoringFunction, 100);
 })();
--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,118 +1,145 @@
 // Data
 // the numbers are NordPool pageIds
 const countryData = {
-    FI: {pageId: 35, hourDiff: 1},
-    DK: {pageId: 41, hourDiff: 0},
-    EE: {pageId: 47, hourDiff: 1},
-    LV: {pageId: 59, hourDiff: 1},
-    LT: {pageId: 53, hourDiff: 1},
-    AT: {pageId: 298578, hourDiff: 0},
-    BE: {pageId: 298736, hourDiff: 0},
-    DE: {pageId: 299565, hourDiff: 0},
-    FR: {pageId: 299568, hourDiff: 0},
-    NL: {pageId: 299571, hourDiff: 0},
-    SE: {pageId: 29, entityName: 'SE3', hourDiff: 0},
-    NO: {pageId: 23, entityName: 'Oslo', hourDiff: 0}
-}
+  FI: { pageId: 35, hourDiff: 1 },
+  DK: { pageId: 41, hourDiff: 0 },
+  EE: { pageId: 47, hourDiff: 1 },
+  LV: { pageId: 59, hourDiff: 1 },
+  LT: { pageId: 53, hourDiff: 1 },
+  AT: { pageId: 298578, hourDiff: 0 },
+  BE: { pageId: 298736, hourDiff: 0 },
+  DE: { pageId: 299565, hourDiff: 0 },
+  FR: { pageId: 299568, hourDiff: 0 },
+  NL: { pageId: 299571, hourDiff: 0 },
+  SE: { pageId: 29, entityName: "SE3", hourDiff: 0 },
+  NO: { pageId: 23, entityName: "Oslo", hourDiff: 0 },
+};
 
 function setTVModel(event) {
-    chrome.storage.sync.set({'tvModel': event.target.value}, function() {
-    });
+  chrome.storage.sync.set({ tvModel: event.target.value }, function () {});
 }
 function updateSelectedTVModel() {
-    chrome.storage.sync.get('tvModel', function(data) {
-        document.getElementById('tvModelSelect').value = data.tvModel;
-    });
+  chrome.storage.sync.get("tvModel", function (data) {
+    document.getElementById("tvModelSelect").value = data.tvModel;
+  });
 }
 function setElPrice(event) {
-    chrome.storage.sync.set({'elPrice': event.target.value}, function() {
-    });
+  chrome.storage.sync.set({ elPrice: event.target.value }, function () {});
 }
 function updateSelectedElPrice() {
-    chrome.storage.sync.get('elPrice', function(data) {
-        document.getElementById('elPrice').value = data.elPrice;
-    });
+  chrome.storage.sync.get("elPrice", function (data) {
+    document.getElementById("elPrice").value = data.elPrice;
+  });
 }
 
 function setCountry(event) {
-    chrome.storage.sync.set({'country': event.target.value}, function() {
-    });
+  chrome.storage.sync.set({ country: event.target.value }, function () {});
 }
 function updateSelectedCountry() {
-    chrome.storage.sync.get('country', function(data) {
-        if (data.country) {
-            document.getElementById('countrySelect').value = data.country;
-        } else {
-            document.getElementById('countrySelect').value = 'FI';
-        }
-    });
+  chrome.storage.sync.get("country", function (data) {
+    if (data.country) {
+      document.getElementById("countrySelect").value = data.country;
+    } else {
+      document.getElementById("countrySelect").value = "FI";
+    }
+  });
 }
 
 async function getCurrentPrice() {
-    chrome.storage.sync.get('country', async function(data) {
-        let country = data.country;
-        if (!country) {
-            country = 'FI';
-        }
-        console.log('Country: ' + country);
-        // in YYYY-MM-DD format
-        let currentCETDate = new Date().toLocaleString( 'sv', { timeZoneName: 'short', timeZone: 'Europe/Berlin' } ).slice(0, 10);
-        let dateDay = currentCETDate.slice(8, 10);
-        let dateMonth = currentCETDate.slice(5, 7);
-        let dateYear = currentCETDate.slice(0, 4);
-        let url = 'https://www.nordpoolgroup.com/api/marketdata/page/' + countryData[country].pageId + '?currency=,EUR,EUR,EUR&endDate=' + dateDay + '-' + dateMonth + '-' + dateYear;
-        chrome.runtime.sendMessage(
-            {
-                contentScriptQuery: "getData"
-                , url: url
-            }, function (jsonData) {
-                console.log(jsonData);
-                let price = getPriceFromJson(jsonData);
-                // Round to 5 decimals
-                console.log('Price: ' + price);
-                // Add the network price to the price
-                let networkPrice = parseFloat(document.getElementById('networkPrice').value);
-                let totalPrice = Math.round((price + (networkPrice || 0.0)) * 10000) / 10000;
-                document.getElementById('elPrice').value = totalPrice;
-                chrome.storage.sync.set({'elPrice': totalPrice}, function() {
-                });
-            });
-    });
+  chrome.storage.sync.get("country", async function (data) {
+    let country = data.country;
+    if (!country) {
+      country = "FI";
+    }
+    console.log("Country: " + country);
+    // in YYYY-MM-DD format
+    let currentCETDate = new Date()
+      .toLocaleString("sv", {
+        timeZoneName: "short",
+        timeZone: "Europe/Berlin",
+      })
+      .slice(0, 10);
+    let dateDay = currentCETDate.slice(8, 10);
+    let dateMonth = currentCETDate.slice(5, 7);
+    let dateYear = currentCETDate.slice(0, 4);
+    let url =
+      "https://www.nordpoolgroup.com/api/marketdata/page/" +
+      countryData[country].pageId +
+      "?currency=,EUR,EUR,EUR&endDate=" +
+      dateDay +
+      "-" +
+      dateMonth +
+      "-" +
+      dateYear;
+    chrome.runtime.sendMessage(
+      {
+        contentScriptQuery: "getData",
+        url: url,
+      },
+      function (jsonData) {
+        console.log(jsonData);
+        let price = getPriceFromJson(jsonData);
+        // Round to 5 decimals
+        console.log("Price: " + price);
+        // Add the network price to the price
+        let networkPrice = parseFloat(
+          document.getElementById("networkPrice").value
+        );
+        let totalPrice =
+          Math.round((price + (networkPrice || 0.0)) * 10000) / 10000;
+        document.getElementById("elPrice").value = totalPrice;
+        chrome.storage.sync.set({ elPrice: totalPrice }, function () {});
+      }
+    );
+  });
 }
 function setNetworkPrice(event) {
-    chrome.storage.sync.set({'networkPrice': event.target.value}, function() {
-    });
+  chrome.storage.sync.set({ networkPrice: event.target.value }, function () {});
 }
 
 function updateNetworkPrice() {
-    chrome.storage.sync.get('networkPrice', function(data) {
-        document.getElementById('networkPrice').value = data.networkPrice;
-    });
+  chrome.storage.sync.get("networkPrice", function (data) {
+    document.getElementById("networkPrice").value = data.networkPrice;
+  });
 }
 
 function getPriceFromJson(jsonData) {
-    // We need timestamp in string format "2022-11-05T10:00:00" Floor to get the hour
-    // Currenthour is 0-23
-    // Should be like this but in CET timezone
-    // new Date().toISOString().slice(0, 13) + ':00:00'
-    let currentCETTime = new Date().toLocaleString( 'sv', { timeZoneName: 'short', timeZone: 'Europe/Berlin' } ).slice(0, 13) + ':00:00'
-    currentCETTime = currentCETTime.replace(' ', 'T');
+  // We need timestamp in string format "2022-11-05T10:00:00" Floor to get the hour
+  // Currenthour is 0-23
+  // Should be like this but in CET timezone
+  // new Date().toISOString().slice(0, 13) + ':00:00'
+  let currentCETTime =
+    new Date()
+      .toLocaleString("sv", {
+        timeZoneName: "short",
+        timeZone: "Europe/Berlin",
+      })
+      .slice(0, 13) + ":00:00";
+  currentCETTime = currentCETTime.replace(" ", "T");
 
-    let row = jsonData.data.Rows.find(row => row.StartTime === currentCETTime);
-    if (row) {
-        // Price is in EUR/MWh
-        return parseFloat(row.Columns[0].Value.replace(',', '.')) / 1000;
-    }
+  let row = jsonData.data.Rows.find((row) => row.StartTime === currentCETTime);
+  if (row) {
+    // Price is in EUR/MWh
+    return parseFloat(row.Columns[0].Value.replace(",", ".")) / 1000;
+  }
 }
 
 (function () {
-    updateSelectedTVModel();
-    updateSelectedElPrice();
-    updateSelectedCountry();
-    updateNetworkPrice();
-    document.getElementById("tvModelSelect").addEventListener("change", setTVModel);
-    document.getElementById("elPrice").addEventListener("change", setElPrice);
-    document.getElementById("countrySelect").addEventListener("change", setCountry);
-    document.getElementById("networkPrice").addEventListener("change", setNetworkPrice);
-    document.getElementById('getPriceButton').addEventListener('click', getCurrentPrice);
+  updateSelectedTVModel();
+  updateSelectedElPrice();
+  updateSelectedCountry();
+  updateNetworkPrice();
+  document
+    .getElementById("tvModelSelect")
+    .addEventListener("change", setTVModel);
+  document.getElementById("elPrice").addEventListener("change", setElPrice);
+  document
+    .getElementById("countrySelect")
+    .addEventListener("change", setCountry);
+  document
+    .getElementById("networkPrice")
+    .addEventListener("change", setNetworkPrice);
+  document
+    .getElementById("getPriceButton")
+    .addEventListener("click", getCurrentPrice);
 })();
